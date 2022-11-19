bd6d89d9570c17ba2f7f64b1733c4dadf0c41f30
Oskar Hahn <mail@oshahn.de>
2022-11-19 12:55:56 +0100

    Fix health status (#81)

--- main.go
+++ main.go
@@ -4,11 +4,10 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"io"
+	"log"
 	"net"
 	"net/http"
 	"os"
-	"strings"
 
 	"github.com/OpenSlides/openslides-autoupdate-service/pkg/auth"
 	"github.com/OpenSlides/openslides-autoupdate-service/pkg/datastore"
@@ -33,12 +32,19 @@ var (
 var cli struct {
 	Run      struct{} `cmd:"" help:"Runs the service." default:"withargs"`
 	BuildDoc struct{} `cmd:"" help:"Build the environment documentation."`
-	Health   struct{} `cmd:"" help:"Runs a health check."`
+	Health   struct {
+		Host     string `help:"Host of the service" short:"h" default:"localhost"`
+		Port     string `help:"Port of the service" short:"p" default:"9007" env:"ICC_PORT"`
+		UseHTTPS bool   `help:"Use https to connect to the service" short:"s"`
+		Insecure bool   `help:"Accept invalid cert" short:"k"`
+	} `cmd:"" help:"Runs a health check."`
 }
 
 func main() {
 	ctx, cancel := environment.InterruptContext()
 	defer cancel()
+
+	icclog.SetInfoLogger(log.Default())
 
 	kongCTX := kong.Parse(&cli, kong.UsageOnError())
 	switch kongCTX.Command() {
@@ -55,7 +61,7 @@ func main() {
 		}
 
 	case "health":
-		if err := contextDone(health(ctx)); err != nil {
+		if err := contextDone(icchttp.HealthClient(ctx, cli.Health.UseHTTPS, cli.Health.Host, cli.Health.Port, cli.Health.Insecure)); err != nil {
 			handleError(err)
 			os.Exit(1)
 		}
@@ -86,40 +92,6 @@ func buildDocu() error {
 	}
 
 	fmt.Println(doc)
-	return nil
-}
-
-func health(ctx context.Context) error {
-	port, found := os.LookupEnv("ICC_PORT")
-	if !found {
-		port = "9007"
-	}
-
-	req, err := http.NewRequestWithContext(ctx, "GET", "http://localhost:"+port+"/system/icc/health", nil)
-	if err != nil {
-		return fmt.Errorf("creating request: %w", err)
-	}
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return fmt.Errorf("sending request: %w", err)
-	}
-
-	if resp.StatusCode != 200 {
-		return fmt.Errorf("health returned status %s", resp.Status)
-	}
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return fmt.Errorf("reading response body: %w", err)
-	}
-
-	expect := `{"healthy": true}`
-	got := strings.TrimSpace(string(body))
-	if got != expect {
-		return fmt.Errorf("got `%s`, expected `%s`", body, expect)
-	}
-
 	return nil
 }
 
